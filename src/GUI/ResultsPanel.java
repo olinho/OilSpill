d36a39c0e191e231d3c4362d2bdeabcc091b9db2
katal24 <talagadawid@gmail.com>
2015-11-02 00:16:41 +0100

    Update ResultsPanel.java

--- src/GUI/ResultsPanel.java
+++ src/GUI/ResultsPanel.java
@@ -1,19 +1,35 @@
 package GUI;
-
+ 
+import model.Agent;
+ 
 import java.awt.Graphics;
-
+import java.util.ArrayList;
+ 
 import javax.swing.JPanel;
-
+ 
 public class ResultsPanel extends JPanel {
-	
-	public ResultsPanel(){
-		super();
-	}
-	
+       
+        public ResultsPanel(){
+                super();
+        }
+       
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(200, 100, 30, 100);
+       // g.fillRect(200, 100, 30, 100);
     }
+    
+    public void drawAgents(ArrayList<Agent> agentList){
+        System.out.println("drawagents");
+        Graphics g  = getGraphics();
 
+      //  g.clearRect(2,8,707,573);// //TODO dac tu rozmiary tego pola w oknieG
+        for(int a=0; a<agentList.size(); a++) {
+            g.drawRect(agentList.get(a).getX(),agentList.get(a).getY(),1,1);
+           
+        }
+        revalidate();
+        //repaint();
+
+    }
 }
