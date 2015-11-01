901c03f44216711b773a8d28d09c98df2cfadaec
katal24 <talagadawid@gmail.com>
2015-11-02 00:15:01 +0100

    Update Algo.java

--- src/model/Algo.java
+++ src/model/Algo.java
@@ -1,44 +1,66 @@
-class Algo {
-    boolean stopSimulation=false;
-    private List<Agent> agentList;
+package model;
+ 
+import GUI.ResultsPanel;
+ 
+import java.util.ArrayList;
+ 
+public class Algo {
+    boolean stopSimulation = false;
+    private ArrayList<Agent> agentList;
  
     public void initialize(int agentNumber) {
-        agentList =  new ArrayList<Agent>();
+        agentList = new ArrayList<Agent>();
         for (int i = 0; i < agentNumber; i++) {
-            Agent ag =  new Agent();
+            Agent ag = new Agent();
             ag.setId(i);
-            ag.setX(0);
-            ag.setY(0);
+            ag.setX(3 + (i % 30));
+            ag.setY(9);
             agentList.add(ag);
         }
     }
-        //K=50000
-    public void simulate(Graphics g,double  xWind,double yWind) {
-        int t=0;
-        while(!stopSimulation){
-           for(int a=0;a<agentList.size();a++){
-               computePosition(agentList.get(a),xWind, yWind);
-           }
-            g.clearRect();// //TODO dac tu rozmiary tego pola w oknie
-            for(int a=0;a<agentList.size();a++) {
-                g.drawRect(agentList.get(a).getX(),agentList.get(a).getX(),1,1);
+ 
+   
+    public void simulate(ResultsPanel panel, double xWind, double yWind) {
+        int t = 0;
+        while (t<1000) {
+            t++;
+            for (int a = 0; a < agentList.size(); a++) {
+                computePosition(agentList.get(a), xWind, yWind);
             }
+            panel.drawAgents(agentList);
+            try {
+                Thread.sleep(75);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
         }
+        System.out.println("koniec simulate");
     }
  
     public void setStopSimulation(boolean stopSimulation) {
         this.stopSimulation = stopSimulation;
     }
  
-    private void computePosition(Agent ag,double xWind,double yWind){
-        double K=50000.0; //TODO ten paramter powinien isc z gui
-        double xDeltaWind = 0.035*yWind;
-        double yDeltaWind = 0.035*xWind;
-        double randomXDelta = Math.random()*2-1+Math.sqrt(K*2*1);
-        double randomYDelta = Math.random()*2-1+Math.sqrt(K*2*1);
+    private void computePosition(Agent ag, double xWind, double yWind) {
+        double K = 50000.0; //TODO ten paramter powinien isc z gui
+        double xDeltaWind = 0.035 * yWind;
+        double yDeltaWind = 0.035 * xWind;
+        
+        double randomXDelta = 0;
+        double randomYDelta = 0;
+       // double randomXDelta = Math.random() * 2 - 1 + Math.sqrt(K * 2 * 1);
+      //  double randomYDelta = Math.random() * 2 - 1 + Math.sqrt(K * 2 * 1);
  
-        ag.setY((int) ((double)ag.getY()+yDeltaWind +randomXDelta));
-        ag.setX((int) ((double) ag.getX() + xDeltaWind + randomYDelta));
+        int seterY = (int) ((double) ag.getY() + yDeltaWind + randomXDelta);
+        int seterX = (int) ((double) ag.getX() + xDeltaWind + randomYDelta);
+        
+        if(seterY > 578) seterY = 578;
+        if(seterX > 707) seterX = 707;
+        ag.setY(seterY);
+        ag.setX(seterX);
+        System.out.println(ag.getX() + " " + ag.getY());
+        
  
     }
 }
