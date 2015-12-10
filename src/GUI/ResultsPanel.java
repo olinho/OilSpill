6f767ab8fb8181d75563721a86e397c07520ee7b
katal24 <talagadawid@gmail.com>
2015-12-10 15:57:10 +0100

    Update ResultsPanel.java

--- src/GUI/ResultsPanel.java
+++ src/GUI/ResultsPanel.java
@@ -37,16 +37,17 @@ public class ResultsPanel extends JPanel {
         
         
         
-   /*     for(int a=0; a<agentList.size(); a++) {
-            g.drawRect(agentList.get(a).getX(),agentList.get(a).getY(),1,1);
-           
-        }
+ //    for(int a=0; a<agentList.size(); a++) {
+   //         g.drawRect(agentList.get(a).getX(),agentList.get(a).getY(),1,1);
+     //      
+       // }
         
-   */
+   
         revalidate();
         //repaint();
 
     }
+    
     double x;
 
     
@@ -54,7 +55,7 @@ public class ResultsPanel extends JPanel {
         Graphics g  = getGraphics();
         int t = 0;
       //  g.clearRect(2,8,707,573);// //TODO dac tu rozmiary tego pola w oknieG
-        g.clearRect(0,0,700,570);
+      //  g.clearRect(0,0,700,570);
         for(int i=0; i<700; i++)
         	for(int j=0; j<570; j++){
         	  		
@@ -93,3 +94,93 @@ public class ResultsPanel extends JPanel {
     }
     	
 }
+
+
+
+
+/*
+package GUI;
+
+import model.Agent;
+ 
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+ 
+
+import javax.swing.JPanel;
+ 
+public class ResultsPanel extends JPanel {
+       
+	Color c = new Color(0,0,0,0);
+	
+        public ResultsPanel(){
+                super();
+        }
+       
+    @Override
+    public void paintComponent(Graphics g) {
+  	      super.paintComponent(g);
+       // g.fillRect(200, 100, 30, 100);
+    }
+    
+    public void drawAgents(ArrayList<Agent> agentList){
+        System.out.println("drawagents");
+        Graphics g  = getGraphics();
+
+      //  g.clearRect(2,8,707,573);// //TODO dac tu rozmiary tego pola w oknieG
+        
+        for(int i=0; i<700; i++)
+        	for(int j=0; j<570; j++){
+        		if (mainGui.table[i][j] > 0)
+        			g.drawRect(i,j,1,1);
+        	}
+        
+        
+        
+        
+   //     for(int a=0; a<agentList.size(); a++) {
+    //        g.drawRect(agentList.get(a).getX(),agentList.get(a).getY(),1,1);
+           
+        }
+        
+   
+        revalidate();
+        //repaint();
+
+    }
+    double x;
+
+    
+    public void draw(){
+        Graphics g  = getGraphics();
+        int t = 0;
+      //  g.clearRect(2,8,707,573);// //TODO dac tu rozmiary tego pola w oknieG
+        g.clearRect(0,0,700,570);
+        for(int i=0; i<700; i++) {
+        	for(int j=0; j<570; j++){
+        	  		
+        		x = mainGui1.getTable2()[i][j];
+					if (x != 0){
+						c = new Color((int)(255-(255/mainGui1.thickness*x)),(int)(255-(255/mainGui1.thickness*x)),(int)(255-(255/mainGui1.thickness*x)));
+        	  			g.setColor(c);
+        			g.drawRect(i,j,1,1);
+        	//		System.out.println((int) (mainGui.table2[i][j]*16));
+					}
+        		
+        		
+        		 
+        }
+        }
+        try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        revalidate();	
+        
+    }
+    	
+}*/
